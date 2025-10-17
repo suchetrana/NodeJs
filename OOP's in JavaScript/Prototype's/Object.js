e23850f139383deac3f51684746dcba1aa8ca987
suchetrana <suchetrana27@gmail.com>
2025-10-17 22:36:55 +0530

    Completed prototype in JS

--- OOP's in JavaScript/Prototype's/Object.js
+++ OOP's in JavaScript/Prototype's/Object.js
@@ -1,1 +1,17 @@
-console.log(Object.prototype.constructor)
+console.log(Object.prototype)
+
+function Product(name, age) {
+    this.name = name;
+    this.age = age;
+}
+const iphone = new Product("Iphone 18 pro", 170000);
+
+
+console.log(iphone);
+Product.prototype.display = function () {
+    console.log("details of product : ", this);
+}
+
+console.log(iphone.display());
+// console.log(iphone.toString());
+
---
+++ OOP's in JavaScript/Prototype's/inheritance_demo.js
@@ -0,0 +1,12 @@
+class Category {
+    constructor(c) {
+        this.categoryname = c;
+    }
+}
+
+class Product extends Category {
+    constructor(n, c) {
+        super(c);
+        this.name = name;
+    }
+}
