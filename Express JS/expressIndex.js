bfe4519ae036076a40d87418aeb700dc42294364
suchetrana <suchetrana27@gmail.com>
2025-10-23 17:42:55 +0530

    Added post api

--- Express JS/expressIndex.js
+++ Express JS/expressIndex.js
@@ -13,6 +13,16 @@ app.get('/home', (request, response) => {
         }
     );
 })
+
+app.post('/user', (request, response) => {
+    const data = request.data;
+    console.log("Incoming data: " + data);
+    response.json(
+        {
+            "message": "successfully received data",
+            "data": data
+        });
+})
 app.listen(PORT, (request, response) => {
     response.send("Hi from express server")
 })
